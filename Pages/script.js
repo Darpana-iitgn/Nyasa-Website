05976cbfa32d98d8a1849a6ef559f0c0d6fae58f
Darpana-iitgn <23110085@iitgn.ac.in>
2024-07-29 15:44:39 +0530

    Update script.js

--- Pages/script.js
+++ Pages/script.js
@@ -44,7 +44,26 @@ popupImage.addEventListener('click', function(event) {
   event.stopPropagation();
 });
 
-
+$(document).ready(function(){
+    $('.carousel').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: true, // Enables navigation arrows
+        infinite: true,
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev">&#10094;</button>',
+        nextArrow: '<button type="button" class="slick-next">&#10095;</button>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });   
+});
 
 
 
